4c40c1fcadcf9041057c63cd4b60ec7c959e56c3
Iury de Oliveira Gomes Figueiredo <ioliveira@id.uff.br>
2018-01-24 15:18:59 +0000

    Adding solution to problem 9.

--- problem-9.go
+++ problem-9.go
@@ -1,1 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func genSqrts(max int) map[float64]int {
+// 	lim := math.Sqrt(1000)
+	sqrts := make(map[float64]int, max)
+
+	for i := 1; i < max; i++ {
+		sqrts[math.Pow(float64(i), 2)] = i
+	}
+
+	return sqrts
+
+}
+
+func IsPerfectSqrt(n float64) (float64, bool) {
+	a := math.Sqrt(n)
+	b := int(a)
+
+	c := math.Pow(float64(b), 2)
+	
+	if c == n {
+		return a, true
+	}
+
+	return a, false
+}
+
+func findProd(f int, sqrts map[float64]int) int{
+	for i, j := range sqrts {
+		for m, n := range sqrts {
+			z := m + i
+			v, err := IsPerfectSqrt(z)
+
+			if  err && j + n + int(v) == f{
+				return j * n * int(v)
+			}
+		}
+	}
+
+	return 0
+	
+}
+
+func PrintSqrts(sqrts map[float64]int) {
+	for key, value := range sqrts {
+		fmt.Println(key, ":", value)
+	}
+}
+
+func main() {
+	sqrts := genSqrts(1000)
+// 	PrintSqrts(sqrts)
+
+// 	fmt.Println(IsPerfectSqrt(17))
+	prod := findProd(1000, sqrts)
+
+	fmt.Println("Result:", prod)
+}
+
+
 
